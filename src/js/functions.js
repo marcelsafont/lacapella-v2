6c94559acb470b82a40120fac734547d78e5642e
okaykendall <kendall.bendheim@gmail.com>
2022-12-07 11:18:48 +0100

    adding slider to project page components

--- src/js/functions.js
+++ src/js/functions.js
@@ -6,34 +6,53 @@
 //     margin: 50,
 //   });
 
-  $(window).on('load', function(){
-    $(".owl-carousel").owlCarousel({
-      items: 1,
-      nav: false,
-      dots: true,
-      margin: 50,
-    });
+$(window).on("load", function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: false,
+    dots: true,
+    margin: 50,
+  });
 
-    $(".des-carousel").slick({
-      infinite: false,
-      variableWidth: false,
-      swipeToSlide: true,
-      arrows: false,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true,
-            centerPadding: "10px",
-          },
+  $(".des-carousel").slick({
+    infinite: false,
+    variableWidth: false,
+    swipeToSlide: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "10px",
         },
-      ],
-    });
-  })
+      },
+    ],
+  });
+  $(".article-images--row").slick({
+    infinite: false,
+    variableWidth: true,
+    swipeToSlide: true,
+    arrows: false,
+    dots: false,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: false,
+        },
+      },
+    ],
+  });
+});
 
 $(document).ready(function () {
   //dropdown hover intent
@@ -45,24 +64,20 @@ $(document).ready(function () {
   //     $(this).find(".wrapper-menu").removeClass("open-desktop");
   //   }
   // );
-  $('.custom-select select').selectric();
-  $(".menu-item--expanded > a").click(function(e){
+  $(".custom-select select").selectric();
+  $(".menu-item--expanded > a").click(function (e) {
     e.preventDefault();
-    $('.wrapper-menu').removeClass('open-desktop');
-    $(this).parent().find('.wrapper-menu').addClass('open-desktop')
-  })
+    $(".wrapper-menu").removeClass("open-desktop");
+    $(this).parent().find(".wrapper-menu").addClass("open-desktop");
+  });
 
-  $('#search-form').click(function(){
-    $('.views-exposed-form').addClass('show-form');
-  })
+  $("#search-form").click(function () {
+    $(".views-exposed-form").addClass("show-form");
+  });
 
-  $('#close-form').click(function(){
-    $('.views-exposed-form').removeClass('show-form');
-  })
-
-
-
-  
+  $("#close-form").click(function () {
+    $(".views-exposed-form").removeClass("show-form");
+  });
 });
 
 // $(document).ready(function () {
@@ -106,22 +121,24 @@ $(document).ready(function () {
 });
 
 // Article slider toggles
-$(document).ready(function(){
-  $('.toggle-images-btn').click(function(e) {
+$(document).ready(function () {
+  $(".toggle-images-btn").click(function (e) {
     e.preventDefault();
-    $(".toggle-images").toggleClass('closed');
-    $(this).toggleClass('open');
+    $(".toggle-images").toggleClass("closed");
+    $(this).toggleClass("open");
     $(this).hasClass("open")
       ? $(this).children(".text").text("Tancar")
       : $(this).children(".text").text("Obrir");
-    $(this).closest('.borders').toggleClass('opened');
-  })
+    $(this).closest(".borders").toggleClass("opened");
+  });
 
-    $(".toggle-videos-btn").click(function (e) {
-      e.preventDefault();
-      $(".toggle-videos").toggleClass("closed");
-      $(this).toggleClass("open");
-      $(this).hasClass("open") ? $(this).children('.text').text("Tancar") : $(this).children('.text').text("Obrir");
-      $(this).closest(".borders").toggleClass("opened");
-    });
-})
+  $(".toggle-videos-btn").click(function (e) {
+    e.preventDefault();
+    $(".toggle-videos").toggleClass("closed");
+    $(this).toggleClass("open");
+    $(this).hasClass("open")
+      ? $(this).children(".text").text("Tancar")
+      : $(this).children(".text").text("Obrir");
+    $(this).closest(".borders").toggleClass("opened");
+  });
+});
